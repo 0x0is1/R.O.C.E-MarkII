76c785174cf11a129665c24e92dc9f75354ee0dc
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2021-01-14 15:52:26 +0530

    Initial commit

---
+++ libluci.py
@@ -0,0 +1,205 @@
+import requests
+import re
+from bs4 import BeautifulSoup as bs
+import urllib.parse
+import random
+
+
+error_key = {0: "Your browsing activity is empty.", 1: "Error404"}
+
+# collector commands
+
+
+def soup_collector(item_id, item_type):
+    url = "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=" + item_id + "&db=" + item_type + \
+        "&report=genbank&extrafeat=null&conwithfeat=on&retmode=html&tool=portal&withmarkup=on&maxdownloadsize=1000000000000"
+    data = requests.get(url)
+    soup = bs(data.content, 'html.parser')
+
+    # killing javascripts and stylesheets
+    for script in soup(["script", "style"]):
+        script.decompose()
+    return soup
+
+
+def name_collector(soup):
+    span = soup.find_all('span', {'class': 'feature'})[0]
+    return span.get('id').split('_')[1]
+
+# get commands
+
+
+def intro(soup):
+    try:
+        raw_intro = soup.findAll('pre', {"class": 'genbank'})[
+            0].text.replace('<a href="', '').replace('">', ' - ').replace('</a>', '')
+
+        i = re.findall(r'(?<=LOCUS)[\S\s]*(?=COMMENT)', raw_intro)
+        return str(i[0]).replace('  ', '')
+    except Exception as e:
+        print(e)
+        return 'No intro found.'
+
+
+def feature_cds(soup, count):
+    iterator = 0
+    value = []
+
+    name = name_collector(soup)
+    while iterator <= count-1:
+        cds = soup.find('span', {"id": "feature_" +
+                                 name + "_CDS_" + str(iterator)})
+        if cds == None:
+            break
+        value.append(str(cds.text).replace('  ', ''))
+        iterator += 1
+    return value
+
+
+def feature_source(soup, count):
+
+    name = name_collector(soup)
+    value = []
+    iterator = 0
+    while iterator <= count-1:
+        source = soup.find(
+            'span', {"id": "feature_" + name + "_source_" + str(iterator)})
+        if source == None:
+            break
+        value.append(str(source.text).replace('  ', ''))
+        iterator += 1
+    return value
+
+
+def feature_peptide(soup, count):
+
+    name = name_collector(soup)
+    value = []
+    iterator = 0
+    while iterator <= count-1:
+        peptide = soup.find(
+            'span', {"id": "feature_" + name + "_mat_peptide_" + str(iterator)})
+        if peptide == None:
+            break
+        value.append(str(peptide.text).replace('  ', ''))
+        iterator += 1
+    return value
+
+
+def feature_stem_loop(soup, count):
+    value = []
+    iterator = 0
+
+    name = name_collector(soup)
+    while iterator <= count-1:
+        stem_loop = soup.find(
+            'span', {"id": "feature_" + name + "_stem_loop_" + str(iterator)})
+        if stem_loop == None:
+            break
+        value.append(str(stem_loop.text).replace('  ', ''))
+        iterator += 1
+    return value
+
+
+def chain_sequence(soup, count):
+    value = []
+    iterator = 1
+
+    name = name_collector(soup)
+    while iterator <= count*60:
+        source = soup.find(
+            'span', {"id": name + "_" + str(iterator)})
+        if source == None:
+            break
+        iterator += 60
+        value.append(str(source.text).replace('  ', ''))
+    return value
+
+
+def feature_gene(soup, count):
+    value = []
+    iterator = 0
+
+    name = name_collector(soup)
+    while iterator <= count-1:
+        gene = soup.find(
+            'span', {"id": "feature_" + name + "_gene_" + str(iterator)})
+        if gene == None:
+            break
+        value.append(str(gene.text).replace('  ', ''))
+        iterator += 1
+    return value
+
+
+def comment(soup):
+    try:
+        raw_intro = soup.findAll('pre', {"class": 'genbank'})[
+            0].text.replace('<a href="', '').replace('">', ' - ').replace('</a>', '')
+
+        i = re.findall(r'(?<=COMMENT)[\S\s]*(?=FEATURES)', raw_intro)
+        return str(i[0]).replace('   ', '')
+    except Exception:
+        return 'No comments found.'
+
+
+def formal():
+    print('Welcome to the Bio-samples framework')
+    print('National Center for BioTechnology Information')
+    print('(https://www.ncbi.nlm.nih.gov)')
+    print('')
+    print('Enter commands to interact with')
+    print('Type Help to see available commands')
+
+# search commands
+
+
+def search_id_list(name, filter1, filter2, category_type):
+    term = urllib.parse.quote(name)
+
+    cookies = {
+        'ncbi_sid': 'CE8C4638E6FC4D21_099'+str(random.randint(0000, 100000))+'SID',
+    }
+    requests.get('https://www.ncbi.nlm.nih.gov/' +
+                 category_type + '/?term=' + term, cookies=cookies)
+    headers = {
+        'Host': 'www.ncbi.nlm.nih.gov',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Connection': 'close',
+    }
+
+    data = 'EntrezSystem2.PEntrez.' + filter1 + '.' + filter2 + '_ResultsPanel.' + filter2 + '_DisplayBar.Presentation=uilist&EntrezSystem2.PEntrez.' + filter1 + \
+        '.' + filter2 + '_ResultsPanel.Entrez_Pager.CurrPage=1&EntrezSystem2.PEntrez.DbConnector.LastQueryKey=1&EntrezSystem2.PEntrez.DbConnector.Cmd=displaychanged'
+
+    response = requests.post('https://www.ncbi.nlm.nih.gov/' + category_type,
+                             headers=headers, cookies=cookies, data=data, verify=True)
+    soup = bs(response.content, 'html.parser')
+    if error_key[0] in soup.text:
+        return "No result found"
+    if error_key[1] in soup.text:
+        return "Error404 no such items"
+    else:
+        return soup.text
+
+
+def search_detail(name, category_type):
+    term = urllib.parse.quote(name)
+    url = 'https://www.ncbi.nlm.nih.gov/' + category_type + \
+        '/?term=' + term + '&report=docsum&format=text'
+    page = requests.get(url)
+    soup = bs(page.content, 'html.parser')
+    if error_key[0] in soup.text:
+        return "No result found"
+    if error_key[1] in soup.text:
+        return "Error404 no such items"
+    else:
+        return soup.text
+
+
+#sample_id = 469716625  # SARS_CoV-2 sample-id 1798174254
+#sample_type = "nuccore"  # nucleotide
+#print(name_collector(soup_collector(str(sample_id), sample_type)))
+#print(search_id_list('sars', 'Gene', 'Gene', 'gene'))
---
+++ mark2.py
@@ -0,0 +1,173 @@
+from discord.ext.commands import CommandNotFound
+from discord.ext import commands
+import discord
+import libluci
+import os
+
+## default
+sample_id = 1798174254  # SARS_CoV-2 sample-id 1798174254
+sample_type = "nuccore"  # nucleotide
+report_type = "genbank"
+doc_type = "html"
+## 
+soup_validity = True
+soup = libluci.soup_collector(str(sample_id), str(sample_type))
+#print(soup)
+
+def f(x, soup_, count):
+    return {
+        "name": libluci.name_collector(soup_),
+        "overview": libluci.intro(soup_), 
+        "gene": libluci.feature_gene(soup_, count),
+        "stem-loop": libluci.feature_stem_loop(soup_, count), 
+        "peptide": libluci.feature_peptide(soup_, count), 
+        "cds": libluci.feature_cds(soup_, count),
+        "source": libluci.feature_source(soup_, count),
+        "comment": libluci.comment(soup_),
+        "sequence": libluci.chain_sequence(soup_, count),
+        "soup": str(soup.text)
+    }.get(x, ['Command not found!'])
+
+def h(name, value):
+    global sample_type, doc_type, report_type, sample_id
+    if name == 'id':sample_id = value
+    if name == 'report':report_type = value
+    if name == 'doc':doc_type = value
+    if name == 'type':sample_type = value
+    if name == 'reset':sample_id=1798174254;sample_type="nuccore";report_type="genbank";doc_type="html"
+
+
+def k(item_name, value, search_type):
+    if search_type == 'list':
+        if item_name == "gene":
+            return libluci.search_id_list(value, "Gene", "Gene", item_name)
+        else:
+            return libluci.search_id_list(value, item_name.capitalize(), "Sequence", item_name)
+    if search_type == 'detail':
+        return libluci.search_detail(value, item_name)
+
+
+def invite_embed():
+    embed = discord.Embed(title='MarkII Invite',
+                          url='https://discord.com/oauth2/authorize?client_id=798966300448784425&permissions=8&scope=bot',
+                          description='Invite MarkII on your server.')
+    return embed
+
+
+def source_embed():
+    source_code = 'https://github.com/0x0is1/R.O.C.E-MarkII'
+    embed = discord.Embed(title='MarkII Source code',
+                          url=source_code,
+                          description='Visit MarkII source git.')
+    return embed
+
+def help_embed(help_module):
+    embed = discord.Embed(title="Rational Operative Communication Entity - Mark II", color=0x03f8fc)
+    if help_module == 'default':
+        embed.add_field(
+        name="Description:", value="This bot is designed for genetic engineering research purpose.", inline=False)
+        embed.add_field(
+            name="**Commands:**\n", value="`set` : Command used for seting parameters to get data. \n`get` : Command used for getting parameter.\n`search`: command used to make search for genetic materials.\n`option`: command used for getting options saved.\ntype `help <module type>` to get help for specific command.",
+            inline=False)
+        embed.add_field(
+            name="Invite: ", value="get invite link by typing `invite`")
+        embed.add_field(
+            name="Source: ", value="get source code by typing `source`")
+        embed.add_field(
+            name="Credits: ", value="get credits info by typing `credits`")
+        return embed
+    if help_module == 'set':
+        embed.add_field(name="**set help**", value="module available for set command.")
+        embed.add_field(
+            name='Description:', value="use for setting item id and item type.")
+        embed.add_field(name='Example: ', value='`set id 1798174254` and `set type nuccore`')
+        embed.add_field(name='Options for item type:', value="Nucleotide (`nuccore`)\nGenes (`gene`)\nProtein (`protein`)\nProbe (`probe`)\nPopset(`popset`)")
+        embed.add_field(name='Options for item id:', value="use `search <item type> <item name>` to get ids.")
+        return embed
+    if help_module == 'get':
+        embed.add_field(
+            name="**get help**", value="module available for get command.")
+        embed.add_field(
+            name='Description:', value="use for getting data for saved item id and item type.")
+        embed.add_field(
+            name='Example: ', value='`get gene`, `get cds` etc.')
+        embed.add_field(
+            name='Options for get:', value="Name (`name`)\nOverview (`overview`)\nComments (`comment`)\nGene (`gene`)\nStem Loop (`stem-loop`)\nPeptide (`peptide`)\nCDS (`cds`)\nSource (`source`)\nAll(`soup`)")
+        return embed
+    if help_module == 'search':
+        embed.add_field(
+            name="**search help**", value="module available for search command.")
+        embed.add_field(
+            name='Description:', value="use for searching data for specific detail type.")
+        embed.add_field(
+            name='Example: ', value='`search nuccore SARS`, `search gene rept` etc.')
+        embed.add_field(
+            name='Options for search type:', value="Nucleotide (`nuccore`)\nGenes (`gene`)\nProtein (`protein`)\nProbe (`probe`)\nPopset(`popset`)")
+        return embed
+    else:
+        embed.add_field(name='Unknown mudule', value="Info for given module is not available.")
+        return embed
+
+bot = commands.Bot(command_prefix='>')
+
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print('Bot is online.')
+
+@bot.command()
+async def get(ctx, get_module, count=1):
+    if soup_validity and not 'Failed to retrieve sequence' in str(soup):
+        current_module = f(get_module, soup, count)
+        if type(current_module) == str:
+            chunk_len = 1980
+            chunks = [current_module[j:j+chunk_len] for j in range(0, len(current_module), chunk_len)]
+        else:
+            chunks = current_module
+        for i in chunks:
+            if len(i) > 1980:
+                chunks_ = [i[k:k+1980] for k in range(0, len(i), 1980)]
+                for l in chunks_:
+                    await ctx.send(str('```css\n'+str(l)+'\n```'))
+            else:
+                await ctx.send(str('```css\n'+str(i)+'\n```'))
+    else:
+        await ctx.send(soup)
+
+@bot.command()
+async def set(ctx, item_name, value):
+    h(item_name, value)
+    global soup
+    global soup_validity
+    soup = libluci.soup_collector(str(sample_id), str(sample_type))
+    if 'Failed to retrieve sequence' in str(soup):
+        soup_validity = False
+        await ctx.send(str(soup) + '\nIf you are sure about it, try changing item type.')
+    else:
+        await ctx.message.add_reaction('✅')
+
+@bot.command()
+async def search(ctx, item_name, value, search_type='detail'):
+    i = k(item_name, value, search_type)
+    chunks_ = [i[k:k+1980] for k in range(0, len(i), 1980)]
+    for j in chunks_:
+        await ctx.send(str('```css\n'+str(j)+'\n```'))
+
+@bot.command()
+async def option(ctx):
+    j = 'id: '+ str(sample_id) + '\ntype: ' + sample_type + '\nreport: ' + report_type + '\ndoc: ' + doc_type
+    await ctx.send('```css\n'+ j +'\n```')
+
+@bot.command()
+async def help(ctx, module='default'):
+    await ctx.send(embed=help_embed(module))
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        await ctx.send('Command not found.')
+    else:raise error
+
+token = os.environ.get('EXPERIMENTAL_BOT_TOKEN')
+bot.run(token)
