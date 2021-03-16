318cd4846c3d09f541fe9a9a56b790a2420798eb
0x0is1 <0x0is1@protonmail.com>
2021-03-17 00:55:29 +0530

    Delete mark2.py

--- mark2.py
+++
@@ -1,8 +0,0 @@
-import requests
-while True:
-    try:
-        r=requests.get("https://profile-counter.glitch.me/0x0is1/count.svg")
-        print(r.status_code)
-    except:
-         print("error")
-         continue
